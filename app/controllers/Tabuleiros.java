c7f66f8787c6a8c5ad5c3384a0e001091143aff1
MLauracarv <62679076+MLauracarv@users.noreply.github.com>
2021-03-02 20:11:51 -0300

    comentarios de teste

--- app/controllers/Tabuleiros.java
+++ app/controllers/Tabuleiros.java
@@ -35,7 +35,9 @@ public class Tabuleiros extends Controller {
 	}
 
 	public static void indexTabuleiro(Long idSala) {
+		System.out.println("idSala do indexTabuleiro "+idSala);
 		SalaVirtual sala = SalaVirtual.findById(idSala);
+		System.out.println("sala que esta sendo renderizada no indexTabuleiro "+sala);
 		render(sala);
 	}
 
@@ -43,7 +45,7 @@ public class Tabuleiros extends Controller {
 		Tabuleiro tabuleiro1 = Tabuleiro.find("idSalaVirtual = ?", salaVirtual).first();
 		if (tabuleiro1 == null) {
 			System.out.println("");
-			System.out.println("ESSE TABULEIRO NAO EXISTEEEE ");
+			System.out.println("ESSE TABULEIRO NAO EXISTEEEE. entao vamos criar ");
 			System.out.println("");
 		} else {
 			System.out.println("Esse tabuleiro existe. n crie outro!");
@@ -53,15 +55,21 @@ public class Tabuleiros extends Controller {
 	}
 
 	public static void salvarTabuleiro(Long idSalaVirtual, @Valid Tabuleiro t) {
+		System.out.println("passou por salvarTabuleiro");
+		System.out.println("aq esta a idSalaVirtual de salvarTabuleiro "+ idSalaVirtual);
 		if (validation.hasErrors()) {	
 			validation.keep();
 			params.flash();	
 			formTabuleiro(idSalaVirtual, t);
 		}
 		SalaVirtual sala = SalaVirtual.findById(idSalaVirtual);
+		System.out.println("sala encontrada em salvaTabuleiro "+ sala);
 		t.salaVirtual = sala;
+				   
 		t.save();
+	        System.out.println("tabuleiro foi salvo!");
 		long idSala = sala.id;
+	       System.out.println("idSala de salvarTabuleiro "+idSala);
 		renderTemplate("Tabuleiros/indexTabuleiro.html", idSala);
 	}
 }
